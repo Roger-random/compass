e6aeba2cefc69ed108e6e0c6b4986b8db0856db1
Regorlas <roger.random@outlook.com>
2023-04-03 08:12:50 +0000

    Check Magnetometer sensor availability

--- compass/src/app/capability-check/capability-check.component.ts
+++ compass/src/app/capability-check/capability-check.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CompassNeedleComponent } from '../compass-needle/compass-needle.component';
+import { MagnetometerService } from '../magnetometer.service';
 
 @Component({
   selector: 'app-capability-check',
@@ -8,5 +9,15 @@ import { CompassNeedleComponent } from '../compass-needle/compass-needle.compone
 })
 
 export class CapabilityCheckComponent {
+  constructor(private magetometerService:MagnetometerService) {
 
+  }
+
+  public get haveMagnetometerAPI() : boolean {
+    return this.magetometerService.haveAPI;
+  }
+
+  public get haveMagnetometerSensor() : boolean {
+    return this.magetometerService.haveSensor;
+  }
 }
--- compass/src/app/magnetometer.service.ts
+++ compass/src/app/magnetometer.service.ts
@@ -3,7 +3,45 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class MagnetometerService {
 
-  constructor() { }
+export class MagnetometerService {
+  _haveSensor : boolean = false;
+  _mag? : Magnetometer = undefined;
+
+  constructor() {
+    if ('undefined' !== typeof(Magnetometer)) {
+      try {
+        this._mag = new Magnetometer({frequency:10, referenceFrame:'screen'});
+        this._mag.onreading = this.onReading;
+        this._mag.onerror = this.onError;
+        this._mag.start();
+        console.log("Mag started");
+      } catch (e) {
+        console.log('Magnetometer is defined yet creation failed.');
+        if (e && typeof(e) === 'object' && 'message' in e) {
+          console.log(`Magnetometer creation failure: ${e.message}`)
+        }
+      }
+    }
+  }
+
+  onReading() : void {
+    this._haveSensor = true;
+    console.log(`onReading! X ${this._mag?.x}`);
+  }
+
+  onError(e:SensorErrorEvent) : void {
+    console.log(`onError: ${e.error.message}`);
+  }
+
+  public get haveAPI() : boolean {
+    if (undefined === this._mag) {
+      return false;
+    }
+    return true;
+  }
+
+  public get haveSensor() : boolean {
+    return this._haveSensor;
+  }
 }
